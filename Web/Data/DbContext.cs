4fa52b6c96cfacd8a32d0d3dca252dddb75b6d63
Madfarm <anthony.scott1005@gmail.com>
2023-09-12 17:16:00 -0400

    Create DbContext

---
+++ Web/Data/DbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Options;
+using System.Data.Entity;
+using Web.Models;
+
+namespace Web.Data
+{
+    public class DbContext
+    {
+        public DbContext(): base() { }
+        public DbSet<Cat> Cats { get; set; }
+    }
+}
