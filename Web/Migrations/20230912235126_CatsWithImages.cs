d79aeb9519d09ca9a2d222775c371ab317919fa4
Madfarm <anthony.scott1005@gmail.com>
2023-09-12 19:59:28 -0400

    Add link to Cat page on Navbar

---
+++ Web/Migrations/20230912235126_CatsWithImages.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class CatsWithImages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "Cats",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "Cats");
+        }
+    }
+}
