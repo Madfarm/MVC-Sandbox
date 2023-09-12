d74e6ddbe27053e7bd2ad55a1822f2273547683c
Madfarm <anthony.scott1005@gmail.com>
2023-09-12 17:36:58 -0400

    Create Initial migrations for Cat model

--- Web/Controllers/CatController.cs
+++ Web/Controllers/CatController.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.Entity;
+using Web.Data;
 
 namespace Web.Controllers
 {
     public class CatController : Controller
     {
-        public IActionResult Index()
+        private readonly DataContext _db;
+        public CatController(DataContext db) 
         {
-            return View();
+            _db = db;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var cats = await _db.Cats.ToListAsync();
+            return View(cats);
         }
         
         public IActionResult InfiniteCat()
