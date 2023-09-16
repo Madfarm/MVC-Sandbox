fe26d6b90d5839d3fc6352201006eca215e82d35
Madfarm <anthony.scott1005@gmail.com>
2023-09-15 22:22:58 -0400

    Update Cat model to include description

--- Web/Models/Cat.cs
+++ Web/Models/Cat.cs
@@ -5,6 +5,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string? Description { get; set; }
         public int Age { get; set; }
         public string Image { get; set; }
     }
