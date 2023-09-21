f5b46a082c82a27d6f78b2c81ba7066076db7598
Madfarm <anthony.scott1005@gmail.com>
2023-09-21 08:35:40 -0400

    Update Cat model

--- Web/Models/Cat.cs
+++ Web/Models/Cat.cs
@@ -1,12 +1,12 @@
 ﻿namespace Web.Models
 {
-    //Not final version
+    
     public class Cat
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string? Description { get; set; }
         public int Age { get; set; }
-        public string Image { get; set; }
+        public string? Image { get; set; }
     }
 }
