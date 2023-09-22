0980817ed65d8d97bc6548b11d9ab33e47e0c164
Madfarm <anthony.scott1005@gmail.com>
2023-09-22 19:24:37 -0400

    Create a customer user to use in Identity

---
+++ AuthAPI/Models/CustomUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AuthAPI.Models
+{
+    public class CustomUser : IdentityUser
+    {
+        public string Name { get; set; }
+        public string FavoriteBreed { get; set; }
+    }
+}
