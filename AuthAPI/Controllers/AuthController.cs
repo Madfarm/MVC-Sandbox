fb822814cb24e3722bb44c68f29a841d2e96d722
Madfarm <anthony.scott1005@gmail.com>
2023-09-22 19:46:33 -0400

    Add register and login endpoints

---
+++ AuthAPI/Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Register()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login()
+        {
+            return Ok();
+        }
+    }
+}
