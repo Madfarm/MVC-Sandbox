59d31f9170032817dba36b31f344c7e8c629bf0f
Madfarm <anthony.scott1005@gmail.com>
2023-09-22 19:31:09 -0400

    Add the user extension model to the db context

--- AuthAPI/Data/DataContext.cs
+++ AuthAPI/Data/DataContext.cs
@@ -1,13 +1,16 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using AuthAPI.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 
 namespace AuthAPI.Data
 {
-    public class DataContext: IdentityDbContext<IdentityUser>
+    public class DataContext: IdentityDbContext<CustomUser>
     {
         public DataContext(DbContextOptions<DataContext> options): base(options) { }
+
+        public DbSet<CustomUser> Users { get; set; }
        
     }
 }
