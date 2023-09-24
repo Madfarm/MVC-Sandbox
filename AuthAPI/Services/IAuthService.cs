7bf320056b1f2b9edc89025d3270a0c394005a1d
Madfarm <anthony.scott1005@gmail.com>
2023-09-24 19:29:37 -0400

    Create the auth interface

---
+++ AuthAPI/Services/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using AuthAPI.Models.Dto;
+
+namespace AuthAPI.Services
+{
+    public interface IAuthService
+    {
+        public Task<UserDto> Register(RegistrationRequestDto registrationRequestDto);
+        public Task<LoginResponse> Login(LoginRequestDto loginRequestDto);
+    }
+}
