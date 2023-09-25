facd4151ead1190f6cea39a102fb16551a268fbd
Madfarm <anthony.scott1005@gmail.com>
2023-09-24 20:37:19 -0400

    Add register method to AuthService

---
+++ AuthAPI/Services/AuthService.cs
@@ -0,0 +1,59 @@
+﻿using AuthAPI.Data;
+using AuthAPI.Models;
+using AuthAPI.Models.Dto;
+using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace AuthAPI.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly DataContext _db;
+        private readonly UserManager<CustomUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AuthService(DataContext db, UserManager<CustomUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+        public Task<LoginResponse> Login(LoginRequestDto loginRequestDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<UserDto> Register(RegistrationRequestDto registrationRequestDto)
+        {
+            CustomUser user = new()
+            {
+                Name = registrationRequestDto.Name,
+                Email = registrationRequestDto.Email,
+            };
+
+            try
+            {
+                var result = await _userManager.CreateAsync(user, registrationRequestDto.Password);
+
+                if(result.Succeeded)
+                {
+                    var returnedUser = _db.Users.First(u => u.UserName == registrationRequestDto.Email);
+
+                    UserDto userDto = new()
+                    {
+                        Id = returnedUser.Id,
+                        Name = returnedUser.Name
+                    };
+
+                    return userDto;
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+
+            return new UserDto();
+        }
+    }
+}
