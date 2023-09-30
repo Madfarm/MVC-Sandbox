361b7539fc25aab8e7330322ff3d40d1443094ce
Madfarm <anthony.scott1005@gmail.com>
2023-09-29 21:30:02 -0400

    Create webdriver for Selenium tests

--- SeleniumTesting/UnitTest1.cs
+++ SeleniumTesting/UnitTest1.cs
@@ -2,9 +2,15 @@ namespace WebTests
 {
     public class Tests
     {
+        //Hooks in NUnit
         [SetUp]
         public void Setup()
         {
+            //Browser driver
+            IWebDriver webDriver = new ChromeDriver();
+
+            //Navigate to site
+            webDriver.Navigate().GoToUrl("https://localhost:7043/");
         }
 
         [Test]
