32773abf4f8e5d6aaabb51764e4669ef95ec5384
Madfarm <anthony.scott1005@gmail.com>
2023-10-04 00:53:01 -0400

    Register the token generator in the container

--- AuthAPI/Program.cs
+++ AuthAPI/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddIdentity<CustomUser, IdentityRole>().AddEntityFrameworkStore
     .AddDefaultTokenProviders();
 
 builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
