22d82c641a2581faa242fb465bc631c2572a1787
Madfarm <anthony.scott1005@gmail.com>
2023-10-04 00:47:37 -0400

    Set up Jwt token generator

---
+++ AuthAPI/Services/JwtTokenGenerator.cs
@@ -0,0 +1,10 @@
+﻿namespace AuthAPI.Services
+{
+    public class JwtTokenGenerator : IJwtTokenGenerator
+    {
+        public string GenerateToken()
+        {
+            throw new NotImplementedException();S
+        }
+    }
+}
