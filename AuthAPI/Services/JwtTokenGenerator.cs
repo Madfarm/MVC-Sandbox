0fd640c569c65926244ee8817f97961410af37c8
Madfarm <anthony.scott1005@gmail.com>
2023-10-04 00:57:49 -0400

    Set up the claims list and token descriptor for the Jwt Token generation

--- AuthAPI/Services/JwtTokenGenerator.cs
+++ AuthAPI/Services/JwtTokenGenerator.cs
@@ -1,5 +1,8 @@
 ﻿using AuthAPI.Models;
+using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace AuthAPI.Services
 {
@@ -12,9 +15,27 @@ namespace AuthAPI.Services
             _jwtOptions = jwtOptions;
         }
 
-        public string GenerateToken()
+        public string GenerateToken(CustomUser customUser)
         {
             var tokenHandler = new JwtSecurityTokenHandler();
+
+            var key = Encoding.ASCII.GetBytes(_jwtOptions.Secret);
+
+            var claimList = new List<Claim>()
+            {
+                new Claim(JwtRegisteredClaimNames.Email, customUser.Email),
+                new Claim(JwtRegisteredClaimNames.Sub, customUser.Id),
+                new Claim(JwtRegisteredClaimNames.Name, customUser.UserName)
+            };
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Audience = _jwtOptions.Audience,
+                Issuer = _jwtOptions.Issuer,
+                Subject = new ClaimsIdentity(claimList),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
         }
     }
 }
