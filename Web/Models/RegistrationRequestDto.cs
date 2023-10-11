22e9559a4adea35a0d94b7435d98818f5d9ac423
Madfarm <anthony.scott1005@gmail.com>
2023-10-11 19:56:55 -0400

    Add the models we need for interacting with the auth api

---
+++ Web/Models/RegistrationRequestDto.cs
@@ -0,0 +1,12 @@
+﻿namespace AuthAPI.Models.Dto
+{
+    // Add a new attribute
+    public class RegistrationRequestDto
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+
+    }
+}
