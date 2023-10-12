54ceda4149723f6522b004d41b5723d3bc725ce3
Madfarm <anthony.scott1005@gmail.com>
2023-10-12 15:26:45 -0400

    Update appsettings with the driver to use

---
+++ SeleniumTesting/Utilities/DriverFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+
+namespace WebTests.Utilities
+{
+    public enum DriverToUse
+    {
+        InternetExplorer,
+        Chrome,
+        Firefox
+    }
+    public class DriverFactory
+    {
+        public IWebDriver Create()
+        {
+            IWebDriver driver;
+
+            var driverToUse = Config.BaseUrl;
+
+            switch(driverToUse)
+            {
+                case DriverToUse.Chrome:
+
+            }
+        }
+       
+
+        
+    }
+}
