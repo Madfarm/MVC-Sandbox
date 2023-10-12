438511186e8938784aca52f56468c82ce2e18ca8
Madfarm <anthony.scott1005@gmail.com>
2023-10-12 16:05:24 -0400

    Create setup and tear down for test suite

---
+++ SeleniumTesting/Tests/Tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebTests.Utilities;
+
+namespace WebTests.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [SetUp]
+        public void startUpTest()
+        {
+            Browser.Init();
+        }
+
+        [TearDown]
+        public void endTest()
+        {
+            Browser.Close();
+        }
+    }
+}
