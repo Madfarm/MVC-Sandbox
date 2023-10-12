607ea42ec15eb931b489ec65c1073857451c86ed
Madfarm <anthony.scott1005@gmail.com>
2023-10-11 20:25:16 -0400

    Add a class for containing our base urls for apis and an enum for HTTP verbs

---
+++ Web/Utility/SD.cs
@@ -0,0 +1,11 @@
+﻿namespace Web.Utility
+{
+    public class SD
+    {
+        public static string AuthAPIBase { get; set; }
+        public enum ApiType
+        {
+            GET, POST, PUT, DELETE
+        }
+    }
+}
