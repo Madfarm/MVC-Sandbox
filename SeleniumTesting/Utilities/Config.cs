ba890d97e6ae669b1d937e8ec99963848a74c96e
Madfarm <anthony.scott1005@gmail.com>
2023-10-12 15:21:16 -0400

    Add a config file and add the base url

---
+++ SeleniumTesting/Utilities/Config.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebTests.Utilities
+{
+    public class Config
+    {
+        public static string BaseUrl => GetValue("BaseUrl");
+
+        private static string GetValue(string value)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            return builder.Build()[value];
+        }
+    }
+}
