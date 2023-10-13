9c5ec45af637ccf45aae98e3d4af1053da207245
Madfarm <anthony.scott1005@gmail.com>
2023-10-12 21:57:47 -0400

    Inject the homepage into the first test suite and write test for navbar is displayed

---
+++ SeleniumTesting/Tests/NavigationAndDisplayTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebTests.PageObjects;
+
+namespace WebTests.Tests
+{
+    [TestFixture]
+    public class NavigationAndDisplayTests : TestBase
+    {
+        private readonly HomePage _homePage;
+        public NavigationAndDisplayTests(HomePage homepage)
+        {
+            _homePage = homepage;
+        }
+        [Test]
+        public void NavBarDisplayedShouldPass()
+        {
+            Assert.That(_homePage.navBar.Displayed, Is.True);
+        }
+        
+    }
+}
