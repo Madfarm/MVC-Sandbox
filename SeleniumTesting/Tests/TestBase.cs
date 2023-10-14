f4a2944c6a65a97e4d28cbfb75cda47c8fa492a7
Madfarm <anthony.scott1005@gmail.com>
2023-10-13 23:51:26 -0400

    Create an instance of the reports manager in the one time setup

--- SeleniumTesting/Tests/TestBase.cs
+++ SeleniumTesting/Tests/TestBase.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebTests.Reporting;
 using WebTests.Utilities;
 
 namespace WebTests.Tests
@@ -11,10 +12,12 @@ namespace WebTests.Tests
     public class TestBase
     {
         protected IWebDriver _driver;
+        protected static ReportsManager Reports;
         [OneTimeSetUp]
         public void startUpTest()
         {
             _driver = new DriverFactory().Create();
+            Reports = new ReportsManager(Config.DriverToUse, Config.BaseUrl);
             _driver.Navigate().GoToUrl(Config.BaseUrl);
         }
 
