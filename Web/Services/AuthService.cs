bca630833b48aeda1be72cccfe5adb43ae5fd704
Madfarm <anthony.scott1005@gmail.com>
2023-10-17 22:20:48 -0400

    Scaffold auth service

---
+++ Web/Services/AuthService.cs
@@ -0,0 +1,18 @@
+﻿using AuthAPI.Models.Dto;
+using Web.Services.IServices;
+
+namespace Web.Services
+{
+    public class AuthService : IAuthService
+    {
+        public Task<LoginResponse?> Login(LoginRequestDto request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<RegistrationRequestDto> Register(RegistrationRequestDto request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
