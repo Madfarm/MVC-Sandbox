13c749a02e4141b783641aaceb28d2a18f2894b3
Madfarm <anthony.scott1005@gmail.com>
2023-10-17 21:35:11 -0400

    Add interface for the auth service

---
+++ Web/Services/IServices/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using AuthAPI.Models.Dto;
+
+namespace Web.Services.IServices
+{
+    public interface IAuthService
+    {
+        Task<LoginResponse?> Login(LoginRequestDto request);
+        Task<RegistrationRequestDto> Register(RegistrationRequestDto request);
+    }
+}
