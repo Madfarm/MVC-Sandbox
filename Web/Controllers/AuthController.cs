6634170139ab327b8873a48bc49223fe4bf70de6
Madfarm <anthony.scott1005@gmail.com>
2023-10-18 13:46:58 -0400

    Add login and register action method

--- Web/Controllers/AuthController.cs
+++ Web/Controllers/AuthController.cs
@@ -4,6 +4,15 @@ namespace Web.Controllers
 {
     public class AuthController : Controller
     {
-        
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
     }
 }
