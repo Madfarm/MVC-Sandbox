8bbcec7fa488e521d3b7a8f743b4a04ac36a8c5f
Madfarm <anthony.scott1005@gmail.com>
2023-10-20 17:41:29 -0400

    Add customer and admin role to static definitions

--- Web/Controllers/AuthController.cs
+++ Web/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Web.Controllers
 {
@@ -6,6 +7,11 @@ namespace Web.Controllers
     {
         public IActionResult Register()
         {
+            List<SelectListItem> roleList = new()
+            {
+                
+            };
+
             return View();
         }
 
--- Web/Utility/SD.cs
+++ Web/Utility/SD.cs
@@ -3,6 +3,9 @@
     public class SD
     {
         public static string AuthAPIBase { get; set; }
+
+        public static string CustomerRole { get; set; } = "CUSTOMER";
+        public static string AdminRole { get; set; } = "ADMIN";
         public enum ApiType
         {
             GET, POST, PUT, DELETE
